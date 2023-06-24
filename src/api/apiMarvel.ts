df5d7ddcaad7eadc8935e52bc2ba281afef3ce55
André Lomba <andrelombagr@gmail.com>
2023-06-23 22:29:28 -0300

    Co-authored-by: lucasbravosilva13@gmail.com <lucasbravosilva13@gmail.com> Co-authored-by: devyas25 <devyas25@users.noreply.github.com> Co-authored-by: Keren Martins Santiago <KerenMS@users.noreply.github.com> Co-authored-by: sheila.moraes@live.com <sheila.moraes@live.com>

---
+++ src/api/apiMarvel.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import md5 from "md5";
+
+const baseUrl: string = "http://gateway.marvel.com/v1/public/";
+
+const publicKey: string = "13b01a0ea5a227eb0a97669df301fb40";
+const privateKey: string = "23abac0e385ec44e34fda261bc0833c7ad813e1b";
+
+const time: string = String(new Date());
+
+const hash: string = md5(time + privateKey + publicKey);
+
+export function getPersonagens(posicao: number) {
+  axios
+    .get(
+      `${baseUrl}characters?ts=${time}&apikey=${publicKey}&hash=${hash}&limit=100&offset=${posicao}`
+    )
+    .then((response) => console.log(time, response.data.data))
+    .catch((err) => console.log(err));
+}
